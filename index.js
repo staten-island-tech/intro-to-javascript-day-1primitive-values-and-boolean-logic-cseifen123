de5dd40c3e5659f24a02e5f6ba9f386ccbf63c5b
cseifen123 <caroline.seifen22@sitechhs.com>
2020-03-24 20:34:19 -0400

    intro to javascript

---
+++ index.js
@@ -0,0 +1,95 @@
+
+
+
+
+
+///////CLASS NOTES BELOW
+
+///////////////strings
+// console.log("Connect");
+// let string="string";
+// const edis ="Edis";
+// let sentence = "Hello " + "Vicky";
+// let sentence2 = " and also Angel";
+// let combine = sentence + sentence2; 
+// combine = `${sentence2}${sentence}`; //redifinging variable
+
+// let student1 = "Sandra";
+// let student2 = "Kelly";
+// let courseName = "AP CSP";
+// let teacherName = "Whalen";
+// let longSentence = `${student1} and ${student2} will be taking ${courseName} with ${teacherName} next year`;
+
+// "next year with" + 
+// teacherName;
+// const LongSentence = '${student1} and ${student2} are amazing adn will be taking ${courseName} with $teacherName}'; 
+// let url = ${url}${movie}
+
+
+
+
+/////////////////number of integers
+// let year = 2020; //different than string "2020"
+// let nextYear= 2021;
+// let past = 2020/2;
+// let farFuture = 2020 * 50; // javascript mathematical operators
+// console.log(past, lastYear, farFuture);//console log differnet varibales if things srent working, do this a lot
+
+
+////////////////////BooLeans
+// let trueStatement = true;
+// let falseStatement = false; 
+
+/////////////////Null and Undefined
+// let undefinedVar;
+// // let Ron; //declared but not defined
+// // console.log(Ron);
+
+// console.log(nonExistent); //null never declared
+
+
+////////////Boolean Logic
+//let student = "Caroline";
+//let age = 15;
+// console.log(student === "Caroline");//true
+// console.log(student === "caroline");//false
+// console.log(student == "caroline");//false
+//console.log(age === 15);
+//console.log(age === "15"); //false dont need quotes because it is not a STRING
+//console.log(age == "15");//true double equals is less strict
+
+// let age = 16;
+// let name = "dog"
+// console.log(age < 17);
+// console.log(age <= 17);
+// console.log(age >= 17);
+//console.log(age == 16 || age == 17); //true
+//console.log(age == 16 && name == "dog"); //true
+//console.log(age !== 16); //if age is not equal to 16
+
+
+
+// console.log(age <= 15 );
+// console.log(age <= 15 || age > 19);//true
+// console.log(age <= 15 && age > 19);//false
+
+
+///////////If statements
+// let age = 19;
+// if(age < 18){
+//     console.log("You are a child");
+// } else if(age >=18){
+//     console.log("You are an adult");
+// }
+
+// let school= "";
+// if(school === "Elementary"){
+//     console.log("You are in k-5");
+// } else if(school === "Middle") {
+//     console.log("you are in 6,7 or 8th grade");
+// } else{
+//     console.log("you are in HS");
+// }
+
+
+
